09b1e4c97f38baa8fad4246fed3d2bffd005ff04
Mohammed Usman <imailusman07@gmail.com>
2025-07-02 23:45:49 +0530

    Created AssetRepository for DB access operations

---
+++ backend/src/main/java/com/military/asset/backend/repository/AssetRepository.java
@@ -0,0 +1,14 @@
+package com.military.asset.backend.repository;
+
+import com.military.asset.backend.entity.Asset;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AssetRepository extends JpaRepository<Asset, Long> {
+
+    List<Asset> findByBaseLocation(String baseLocation);
+    List<Asset> findByType(String type);
+}
